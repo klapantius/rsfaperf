a085a1ff491add7a8c4a11b6b1d7fc33de9450af
JuBa <cloud9@klapantius.net>
2017-08-04 08:54:30 +0000

    added webui.js and dependency to rest-flat-file-db

--- storage.js
+++ storage.js
@@ -2,7 +2,8 @@ const restflat = require('rest-flat-file-db');
 const flatdb = require('flat-file-db');
  
 const app = restflat(flatdb.sync('/tmp/mydatabase'));
- 
-// this is just a normal koa2 app and it is ready to launch. 
+     
+app.use(ctx => {
+});
  
 app.listen();
