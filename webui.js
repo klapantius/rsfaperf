77299f5e6354531a402b41670437422e8d7f8aa3
klapantius <balazs.juhasz@klapantius.net>
2017-08-25 15:14:21 +0200

    removed unused linq dependency

--- webui.js
+++ webui.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var db = require('./storage.js');
-//var LINQ = require('node-linq').LINQ;
 
 app.get('/', function (req, res) {
 
